ff6207b304dfb6837bf2a5ca9e4eebbb0dc6970d
zhangjian-github <zhangjian.address@gmail.com>
2018-07-17 01:15:16 +0800

    add sql and readme.md

---
+++ db.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
